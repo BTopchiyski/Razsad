6db4e4edcd11084e3daf26f3aeffdb6bc84c6d6f
B.Topchiyski <topchiisky@gmail.com>
2020-05-04 14:22:48 +0300

    Idduuno

---
+++ Razsad/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Razsad
+{
+    public partial class Form1 : Form
+    {
+        decimal CenaPiper = 0.07M;
+        decimal CenaDomati = 0.30M;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
